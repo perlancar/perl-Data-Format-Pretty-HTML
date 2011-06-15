65633e8004edad5429fd904eeb6032de6e579341
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-06-15 13:18:10 +0700

    first import, v0.01

---
+++ lib/Data/Format/Pretty/HTML.pm
@@ -0,0 +1,260 @@
+package Data::Format::Pretty::HTML;
+# ABSTRACT: Pretty-print data structure for HTML output
+
+=head1 SYNOPSIS
+
+In your program:
+
+ use Data::Format::Pretty::HTML qw(format_pretty);
+ ...
+ print format_pretty($result);
+
+Some example output:
+
+Scalar, format_pretty("foo & bar"):
+
+ foo &amp; bar
+
+Scalar multiline, format_pretty("foo\nbar\nbaz"):
+
+ <pre>foo
+ bar
+ baz</pre>
+
+List, format_pretty([qw/foo bar baz qux/]):
+
+ <table>
+   <tr><td>foo</td></tr>
+   <tr><td>bar</td></tr>
+   <tr><td>baz</td></tr>
+   <tr><td>qux</td></tr>
+ </table>
+
+Hash, format_pretty({foo=>"data",bar=>"format",baz=>"pretty",qux=>"html"}):
+
+ <table>
+   <tr><th>key</th><th>value</th></tr>
+   <tr><td>bar</td><td>format</td></tr>
+   <tr><td>baz</td><td>pretty</td></tr>
+   <tr><td>foo</td><td>data</td></tr>
+   <tr><td>qux</td><td>html</td></tr>
+ </table>
+
+2-dimensional array, format_pretty([ [1, 2, ""], [28, "bar", 3], ["foo", 3,
+undef] ]):
+
+ <table>
+   <tr><th>column0</th><th>column1</th><th>column2</th></tr>
+   <tr><td class="number">1</td><td class="number">2</td><td></td></tr>
+   <tr><td class="number">28</td><td>bar</td><td class="number">3</td></tr>
+   <tr><td>foo</td><td class="number">3</td><td></td></tr>
+ </table>
+
+An array of hashrefs, such as commonly found if you use DBI's fetchrow_hashref()
+and friends, format_pretty([ {a=>1, b=>2}, {b=>2, c=>3}, {c=>4} ]):
+
+ <table>
+   <tr><th>a</th><th>b</th><th>c</th></tr>
+   <tr><td class="number">1</td><td class="number">2</td><td></td></tr>
+   <tr><td></td><td class="number">2</td><td class="number">3</td></tr>
+   <tr><td></td><td></td><td class="number">4</td></tr>
+ </table>
+
+Some more complex data, format_pretty({summary => "Blah...", users =>
+[{name=>"budi", domains=>["f.com", "b.com"], quota=>"1000"}, {name=>"arif",
+domains=>["baz.com"], quota=>"2000"}], verified => 0}):
+
+ <table>
+
+   <tr>
+     <td>summary</td>
+     <td>Blah...</td>
+   </tr>
+
+   <tr>
+     <td>users</td>
+     <td>
+       <table>
+         <tr><th>domains</th><th>name</th><th>quota</th></tr>
+         <tr><td>f.com, b.com</td><td>budi</td><td class="number">1000</td></tr>
+         <tr><td>baz.com</td><td>arif</td><td class="number">2000</td></tr>
+     </td>
+   </tr>
+
+   <tr>
+     <td>verified</td>
+     <td class="number">0</td>
+   </tr>
+
+ </table>
+
+Structures which can't be handled yet will simply be output as YAML,
+format_pretty({a => {b=>1}}):
+
+ <pre>a:
+   b: 1
+ </pre>
+
+
+=head1 DESCRIPTION
+
+This module has the same spirit as L<Data::Format::Pretty::Console> (and
+currently implemented as its subclass). The idea is to throw it some data
+structure and let it figure out how to best display the data in a pretty HTML
+format.
+
+Differences with Data::Format::Pretty::Console:
+
+=over 4
+
+=item *
+
+=item *
+
+=back
+
+This module uses L<Log::Any> for logging.
+
+=cut
+
+use 5.010;
+use strict;
+use warnings;
+use Log::Any '$log';
+
+use Data::Format::Pretty::Console;
+use HTML::Entities;
+use Scalar::Util qw(looks_like_number);
+use URI::Find::Schemeless;
+use YAML::Any;
+
+require Exporter;
+our @ISA = qw(Exporter Data::Format::Pretty::Console);
+our @EXPORT_OK = qw(format_pretty);
+
+=head1 FUNCTIONS
+
+=head2 format_pretty($data, \%opts)
+
+Return formatted data structure as HTML. Options:
+
+=over 4
+
+=item * table_column_orders => [[colname, colname], ...]
+
+See Data::Format::Pretty::Console for more details.
+
+=item * linkify_urls_in_text => BOOL
+
+Whether to convert 'http://foo' in text into '<a
+href="http://foo">http://foo</a>'. Default is true.
+
+=back
+
+=cut
+
+sub format_pretty {
+    my ($data, $opts) = @_;
+    $opts //= {};
+    __PACKAGE__->new($opts)->_format($data);
+}
+
+=for Pod::Coverage new
+
+=cut
+
+# OO interface is hidden
+sub new {
+    my ($class, $opts) = @_;
+    my $obj = $class->SUPER::new($opts);
+    #my $obj = Data::Format::Pretty::Console->new($opts);
+    $obj->{opts}{linkify_urls_in_text} //= 1;
+    $obj->{opts}{interactive} = 1;
+    #bless $class, $obj;
+    $obj;
+}
+
+sub _htmlify {
+    my ($self, $text) = @_;
+
+    $text = encode_entities($text);
+    if ($self->{opts}{linkify_urls_in_text}) {
+        URI::Find::Schemeless->new(
+            sub {
+                #my $uri = encode_entities $_[0];
+                #my $uri = $_[0];
+                my $uri = decode_entities $_[0];
+                return qq|<a href="$uri">$uri</a>|;
+            })->find(\$text);
+    }
+    if ($text =~ /\R/) {
+        return "<pre>$text</pre>";
+    } else {
+        return $text;
+    }
+}
+
+sub _render_table {
+    my ($self, $t) = @_;
+    my @t = ("<table>\n");
+
+    push @t, "  <tr>";
+    for my $c (@{$t->{tbl_cols}}) {
+        push @t, (
+            "<th", (looks_like_number($c) ? ' class="number"':''), ">",
+            $self->_htmlify($c),
+            "</th>",
+        );
+    }
+    push @t, "</tr>\n";
+    for my $r (@{$t->{tbl_rows}}) {
+        push @t, "  <tr>";
+        for my $c (@$r) {
+            push @t, (
+                "<td", (looks_like_number($c) ? ' class="number"':''), ">",
+                $self->_htmlify($c),
+                "</td>",
+            );
+        }
+        push @t, "</tr>\n";
+    }
+    push @t, "</table>\n";
+    join "", @t;
+}
+
+# format unknown structure, the default is to dump YAML structure
+sub _format_unknown {
+    my ($self, $data) = @_;
+    $self->_htmlify(Dump $data);
+}
+
+sub _format_scalar {
+    my ($self, $data) = @_;
+
+    my $sdata = defined($data) ? "$data" : "";
+    $self->_htmlify($sdata);
+}
+
+sub _format_hot {
+    my ($self, $data) = @_;
+    # show hot as paragraphs:
+    #
+    # key:
+    # value (table)
+    #
+    # key2:
+    # value ...
+    my @t;
+    for my $k (sort keys %$data) {
+        push @t, "$k:\n", $self->_format($data->{$k}), "\n";
+    }
+    return join("", @t);
+}
+
+=head1 SEE ALSO
+
+L<Data::Format::Pretty::Console>
+
+=cut
+
+1;
---
+++ t/01-basics.t
@@ -0,0 +1,100 @@
+#!perl -Tw
+use strict;
+use Test::More;
+use Data::Dump::Partial qw(dumpp);
+use Data::Format::Pretty::HTML qw(format_pretty);
+use YAML::Any;
+
+my @data = (
+    # test encoding entities
+    {
+        data         => "a & b",
+        struct       => "scalar",
+        output       => "a &amp; b",
+    },
+
+    # test newline in text triggers <pre>
+    {
+        data         => "a\nb",
+        struct       => "scalar",
+        output       => "<pre>a\nb</pre>",
+    },
+
+    # check html table tag, class=number
+    {
+        data         => [1, "a"],
+        struct       => "list",
+        output_re    => qr!<table>\s*
+                           <tr><th>data</th></tr>\s*
+                           <tr><td\sclass="number">1</td></tr>\s*
+                           <tr><td>a</td></tr>\s*
+                          </table>!sx,
+    },
+
+    # check linkify_urls_in_text
+    {
+        name         => 'opt linkify_urls_in_text',
+        data         => 'go to http://example.com/?a & click the image',
+        output       => 'go to <a href="http://example.com/?a">'.
+          'http://example.com/?a</a> &amp; click the image',
+    },
+);
+
+sub is_yaml {
+    my ($data, $test_name) = @_;
+    eval { Load($data) };
+    ok(!$@, $test_name);
+}
+
+sub isnt_yaml {
+    my ($data, $test_name) = @_;
+    eval { Load($data) };
+    #XXX doesn't die?
+    #ok($@, $test_name);
+    #print "\$data=$data, \$@=$@\n";
+}
+
+ # detect and format
+sub test_dnf {
+    my ($spec) = @_;
+    my $data   = $spec->{data};
+    my $opts   = $spec->{opts} // {};
+    my $struct = $spec->{struct};
+    my $test_name = $spec->{name} //
+        ($struct // "unknown") . ": " . dumpp($data);
+
+    if (exists $spec->{struct}) {
+        my $fmt = Data::Format::Pretty::HTML->new;
+        my ($s, $sm) = $fmt->_detect_struct($data);
+        if (!$struct) {
+            ok(!$s, "$test_name: _detect_struct: structure unknown");
+        } else {
+            is($s, $struct, "$test_name: _detect_struct: structure is ".
+                   "'$struct'");
+        }
+    }
+
+    if (exists($spec->{output}) || exists($spec->{output_re}) ||
+            exists($spec->{is_yaml})) {
+        my $output;
+        {
+            $output = format_pretty($data, $opts);
+        }
+        if (exists($spec->{output})) {
+            is($output, $spec->{output}, "$test_name: output exact match");
+        }
+        if (exists($spec->{output_re})) {
+            like($output, $spec->{output_re}, "$test_name: output regex match");
+        }
+        if (exists($spec->{is_yaml})) {
+            if ($spec->{is_yaml}) {
+                is_yaml($output, "$test_name: is YAML");
+            } else {
+                isnt_yaml($output, "$test_name: is not YAML");
+            }
+        }
+    }
+}
+
+test_dnf($_) for @data;
+done_testing();
